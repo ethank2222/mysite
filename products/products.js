3f4b44d9b2cf1daf26bd6f05355ff10cf57d0794
ethank2222 <ethank2222@gmail.com>
2022-03-07 07:59:41 -0800

    local price database

--- products/products.js
+++ products/products.js
@@ -1,6 +1,6 @@
 let data = {
     "prices": {
-        "dominoes-red": "$14.99",
+        "dominoes-red": "$13.99",
         "dominoes-orange": "$14.99",
         "dominoes-yellow": "$14.99",
         "dominoes-light-green": "$14.99",
@@ -13,14 +13,20 @@ let data = {
         "dominoes-black": "$14.99",
         "dominoes-white": "$14.99",
         
-        "templates": "8.99"
+        "templates": "$8.99"
     }
 }
 
 var productsOnPage = document.getElementsByClassName("product");
 for (var i = 0; i < productsOnPage.length; i++){
-    childDiv = productsOnPage[i].getElementsByClassName('product-actions')[0];
-    childDiv1 = productsOnPage[i].getElementsByTagName('h3')[0];
-    id1 = childDiv1.id;
-    childDiv.firstChild.innerText = data.prices[3];
+    var childDiv = productsOnPage[i].getElementsByClassName('product-actions')[0].getElementsByTagName("p")[0];
+    var childDiv1 = productsOnPage[i].getElementsByTagName('h3')[0];
+    var id1 = childDiv1.id;
+    childDiv.innerText = data.prices[id1];
+}
+if (document.getElementsByClassName("product-description").length > 0 ){
+    var childDiv = document.getElementsByClassName("product-description")[0].getElementsByTagName('div')[0].getElementsByTagName("p")[0];
+    var childDiv1 = document.getElementsByClassName("product-description")[0];
+    var id1 = childDiv1.id;
+    childDiv.innerText = "Price: " + data.prices[id1];
 }
